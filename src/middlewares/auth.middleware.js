2965785e12c7f25efeb1405b3d52b2b80d467a5c
Hassan Ali <139656401+hasanbut17314@users.noreply.github.com>
2024-08-07 01:18:17 +0500

    updated auth middleware to accept token from headers and cookies both

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
-        const token = req.cookies?.accessToken
+        const token = req.cookies?.accessToken || req.headers?.authorization?.split(" ")[1]
 
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
